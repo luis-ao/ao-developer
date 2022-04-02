922427fd3e19a11f735f3aed05f22f8e69d35178
luis-ao <96146916+luis-ao@users.noreply.github.com>
2022-04-01 22:03:13 -0600

    Aplicando Rem y EM

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,23 @@
+/* *********** 
+     MENÚ
+_________________*/
 const menuBtn = document.querySelector(".menu-open");
 const exitBtn = document.querySelector(".menu-exit");
 
 let t1 = gsap.timeline({ paused: true });
-t1.to(".menu", { 
-    opacity: 1, 
-    duration: 1, 
-    top: 0, 
-    ease: Power2.easeInOut 
+t1.to(".menu", {
+    opacity: 1,
+    duration: 1,
+    top: 0,
+    ease: Power2.easeInOut
 });
-t1.to( ".nav-gsap", {
-        opacity: 1,
-        marginBottom: 0,
-        duration: 1,
-        ease: Power2.easeInOut,
-        stagger: 0.3,
-    },
+t1.to(".nav-gsap", {
+    opacity: 1,
+    marginBottom: 0,
+    duration: 1,
+    ease: Power2.easeInOut,
+    stagger: 0.3,
+},
     ">-0.5"
 );
 
@@ -25,4 +28,16 @@ menuBtn.addEventListener("click", () => {
 exitBtn.addEventListener("click", () => {
     t1.timeScale(2.5);
     t1.reverse();
+});
+
+
+/* *********** 
+     PRELOADER 
+_________________*/
+var loader = document.getElementById("loader");
+window.addEventListener("load", function () {
+    loader.style.height = "100%";
+    loader.style.width = "100%";
+    loader.style.borderRadius = "50%";
+    loader.style.visibility = "hidden"
 });
