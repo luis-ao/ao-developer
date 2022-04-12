b8a04d8813fdbf6e8718a355e6dd901b7eb6c9a9
luis-ao <96146916+luis-ao@users.noreply.github.com>
2022-04-12 00:00:18 -0500

    Header + gsap

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,46 @@
 /* *********** 
      MENÚ
 _________________*/
-const menuBtn = document.querySelector(".menu-open");
-const exitBtn = document.querySelector(".menu-exit");
+gsap.from('.header__logo, .menu-open-gsap',1.2,{
+    opacity: 0,
+    y: '-100%',
+    stagger: {
+        amount: .4
+    }
+});
 
-let t1 = gsap.timeline({ paused: true });
+const menuBtn = document.querySelector('.menu-open');
+const exitBtn = document.querySelector('.menu-exit');
 
-t1.to(".menu", { 
+const t1 = gsap.timeline({ paused: true });
+t1.to('.menu', { 
     opacity: 1,
     top: 0, 
     duration: 1, 
     ease: Power2.easeInOut }
 );
-t1.to(".menu__links", {
+t1.to('.menu__links', {
         opacity: 1,
         paddingTop: 0,
         duration: 1,
-        ease: Power3.easeInOut,
+        ease: Power2.easeInOut,
         /* stagger: 0.3, */
     },
-    ">-0.5"
+    '>-0.4'
 );
-t1.to(".menu__background", {
+t1.to('.menu__background', {
         opacity: 0.4,
         duration: 1,
-        ease: "slow(0.7, 0.7, false)",
+        ease: 'slow(0.7, 0.7, false)',
     },
-">-0.6"
+'>-0.6'
 );
 
-menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener('click', () => {
     t1.play().timeScale(1);
 });
 
-exitBtn.addEventListener("click", () => {
+exitBtn.addEventListener('click', () => {
     t1.timeScale(2.2);
     t1.reverse();
 });
@@ -42,10 +49,10 @@ exitBtn.addEventListener("click", () => {
 /* *********** 
      PRELOADER 
 _________________*/
-var loader = document.getElementById("loader");
-window.addEventListener("load", function () {
-    loader.style.height = "100%";
-    loader.style.width = "100%";
-    loader.style.borderRadius = "50%";
-    loader.style.visibility = "hidden"
+var loader = document.getElementById('loader');
+window.addEventListener('load', function () {
+    loader.style.height = '100%';
+    loader.style.width = '100%';
+    loader.style.borderRadius = '50%';
+    loader.style.visibility = 'hidden'
 });
