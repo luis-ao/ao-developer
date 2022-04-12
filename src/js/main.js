2f48e095fd16e88862314eb027bc69ad3e5d8f19
luis-ao <96146916+luis-ao@users.noreply.github.com>
2022-04-11 22:29:20 -0500

    Animaciones con GSAP

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,8 @@ let t1 = gsap.timeline({ paused: true });
 
 t1.to(".menu", { 
     opacity: 1,
-    duration: 1, 
     top: 0, 
+    duration: 1, 
     ease: Power2.easeInOut }
 );
 t1.to(".menu__links", {
@@ -17,13 +17,13 @@ t1.to(".menu__links", {
         paddingTop: 0,
         duration: 1,
         ease: Power3.easeInOut,
+        /* stagger: 0.3, */
     },
     ">-0.5"
 );
 t1.to(".menu__background", {
         opacity: 0.4,
         duration: 1,
-        stagger: 0.3,
         ease: "slow(0.7, 0.7, false)",
     },
 ">-0.7"
@@ -34,7 +34,7 @@ menuBtn.addEventListener("click", () => {
 });
 
 exitBtn.addEventListener("click", () => {
-    t1.timeScale(2.5);
+    t1.timeScale(2.2);
     t1.reverse();
 });
 
