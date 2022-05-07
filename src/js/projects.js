6bec023c87cec921bfae3b6ea99a0f5484fd102e
luis-ao <96146916+luis-ao@users.noreply.github.com>
2022-05-07 11:55:03 -0500

    Se agrega página de proyectos

---
+++ src/js/projects.js
@@ -0,0 +1,39 @@
+const tl = gsap.timeline();
+
+tl.to(".titleGSAP", {
+  duration: 1,
+  opacity: 1,
+});
+tl.from(
+  ".line",
+  {
+    duration: 1,
+    stagger: {
+      amount: 0.5,
+    },
+    width: "0%",
+  },
+  "-=.2"
+);
+tl.from(
+  ".projects__nav-name",
+  {
+    duration: 1,
+    stagger: {
+      amount: 0.5,
+    },
+    y: 100,
+  },
+  "-=1.3"
+);
+tl.from(
+  ".projects__nav-sub",
+  {
+    duration: 1,
+    stagger: {
+      amount: 0.5,
+    },
+    y: 120,
+  },
+  "-=1.1"
+);
