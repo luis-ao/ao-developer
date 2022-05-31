4ec524fd0d0777f36e9a52f38f0da6de81d50bd5
luis-ao <96146916+luis-ao@users.noreply.github.com>
2022-05-31 12:58:57 -0500

    Caja de Luz parte 1

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,16 +1,28 @@
+/* const btnNext = document.querySelector('#btn-next');
+const btnPrev = document.querySelector('#btn-prev'); */
+
 const btnClose = document.querySelector('#btn-close');
-const btnNext = document.querySelector('#btn-next');
-const btnPrev = document.querySelector('#btn-prev');
-const img = document.querySelector('#img-galery');
-const lightbox = document.querySelector('#lightbox-principal');
+
+const lightbox = document.querySelector('#lightbox');
+const img = document.querySelectorAll('#img-galery');
 const imgActiva = document.querySelector('#img-activa');
-let indiceImg = 0;
 
-const abreLightbox = () => {
-imgActiva.src = envent.target.src;
-lightbox.style.display = 'flex';
-};
-
-img.forEach((imagen) => {
-    imagen.addEventListener('click', abreLightbox);
+img.forEach( img =>{
+    img.addEventListener('click', ()=>{
+        addImg(img.getAttribute('src'),img.getAttribute('alt'));
+    })
+    
 });
+
+const addImg = (srcImg, altImg)=>{
+    lightbox.classList.toggle('move');
+    imgActiva.classList.toggle('show');
+    imgActiva.src = srcImg;
+    imgActiva.alt = altImg;
+}
+
+lightbox.addEventListener('click', ()=>{
+    lightbox.classList.toggle('move');
+    imgActiva.classList.toggle('show');
+})
+
